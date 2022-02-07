261c4971d81764b67119a5eb3e992d384a251295
Austin McPhail <austin@mcphail.dev>
2022-02-06 19:02:43 -0600

    fix: 🐛 update script and fix logic for migrations

--- packages/back/src/app.ts
+++ packages/back/src/app.ts
@@ -18,33 +18,39 @@ server.register(fastifyMySql, {
 });
 server.register(async (fastify, _, next) => {
 	// check if db exists and contains pokemon table
-	const result = (
+	let result = (
 		await fastify.mysql.query(
 			`SELECT table_rows FROM information_schema.tables WHERE table_schema = 'squirdle' AND table_name = 'pokemon'`
 		)
 	)[0] as { table_rows: number }[];
-	if (result.length === 0) return next();
-	const rows = result[0].table_rows;
-	if (rows > 0) return next();
-	// seed db
-	let { error } = await seedDB(fastify);
-	if (error) throw new Error('Failed to seed database');
+	if (!result || result.length === 0 || result[0].table_rows <= 0) {
+		// seed db
+		const { error } = await seedDB(fastify);
+		if (error) throw new Error('Failed to seed database');
+	}
 
 	// add daily table
-	({ error } = await doQuery(
-		fastify,
-		`CREATE TABLE IF NOT EXISTS daily (id INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, day Date UNIQUE, PRIMARY KEY (id), FOREIGN KEY (name) REFERENCES pokemon(name))`
-	));
-	if (error) throw error;
+	result = (
+		await fastify.mysql.query(
+			`SELECT table_rows FROM information_schema.tables WHERE table_schema = 'squirdle' AND table_name = 'daily'`
+		)
+	)[0] as { table_rows: number }[];
+	if (!result || result.length === 0 || result[0].table_rows <= 0) {
+		const { error } = await doQuery(
+			fastify,
+			`CREATE TABLE IF NOT EXISTS daily (id INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, day Date UNIQUE, PRIMARY KEY (id), FOREIGN KEY (name) REFERENCES pokemon(name))`
+		);
+		if (error) throw error;
 
-	const { data: randomPokemon, error: randomPokemonError } = await getRandomPokemon(fastify);
-	if (randomPokemonError || !randomPokemon) {
-		throw randomPokemonError || new Error('Could not get a random pokemon');
+		const { data: randomPokemon, error: randomPokemonError } = await getRandomPokemon(fastify);
+		if (randomPokemonError || !randomPokemon) {
+			throw randomPokemonError || new Error('Could not get a random pokemon');
+		}
+		await doQuery(fastify, `INSERT IGNORE INTO daily (name, day) VALUES (?, CURRENT_DATE())`, [
+			randomPokemon.name
+		]);
+		await updatePokemon(fastify, randomPokemon);
 	}
-	await doQuery(fastify, `INSERT IGNORE INTO daily (name, day) VALUES (?, CURRENT_DATE())`, [
-		randomPokemon.name
-	]);
-	await updatePokemon(fastify, randomPokemon);
 	next();
 });
 server.register(router);
